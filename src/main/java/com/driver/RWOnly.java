44eaa845926d9df5496f717ae56e4e7f4329e856
Aman Anand Tirpude <79367645+amantirpude@users.noreply.github.com>
2022-10-05 21:09:58 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void getName(String name){
+        System.out.println(name);
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
