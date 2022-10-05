e058f109e278aae470caf07ff7642495d2d2f9ee
Aman Anand Tirpude <79367645+amantirpude@users.noreply.github.com>
2022-10-05 21:45:25 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,13 @@
+
 package com.driver;
 
 public class Main {
-  RWOnly rw = new RWOnly(Aman);
-  rw.name;
+  public static void main(String[] args){
+    RWOnly rw = new RWOnly();
+    //rw.name = "Aman";
+    //System.out.println(rw.name);
+    //java: name has private access in com.driver.RWOnly
+    rw.setName("Aman");
+    System.out.println(rw.getName());
+  }
 }
